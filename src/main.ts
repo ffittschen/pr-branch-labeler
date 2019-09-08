e9dbb6665e74fb813b1aefc492b6c8fbf4da9a77
Florian Fittschen <ffittschen@gmail.com>
2019-09-08 16:35:51 +0200

    Handle new type of config

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,62 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 import { getConfig } from "./config";
+import { ConfigEntry } from "./ConfigEntry";
 import matcher from "matcher";
 
 const CONFIG_FILENAME = "pr-labeler.yml";
-const defaults = {
-  feature: "feature/*",
-  fix: ["bugfix/*", "hotfix/*"],
-  chore: "chore/*"
-};
+const defaults: ConfigEntry[] = [
+  { label: "feature", head: "feature/*", base: undefined },
+  { label: "bugfix", head: ["bugfix/*", "hotfix/*"], base: undefined },
+  { label: "chore", head: "chore/*", base: undefined }
+];
 
-const githubToken = core.getInput("someToken");
-const octokit = new github.GitHub(githubToken);
+// Export the context to be able to mock the payload during tests.
+export const context = github.context;
 
-async function run() {
+export async function run() {
   try {
-    const context = github.context;
-    if (
-      context &&
-      context.payload &&
-      context.payload.repository &&
-      context.payload.pull_request
-    ) {
-      const repoInfo = {
-        owner: context.payload.repository.owner.login,
-        repo: context.payload.repository.name
-      };
-      const ref = context.payload.pull_request.head.ref;
-      const config = {
-        ...defaults,
-        ...(await getConfig(octokit, CONFIG_FILENAME, repoInfo))
-      };
-      const labelsToAdd = Object.entries(config).reduce(
-        (labels: string[], [label, patterns]) => {
-          if (
-            Array.isArray(patterns)
-              ? patterns.some(pattern => matcher.isMatch(ref, pattern))
-              : matcher.isMatch(ref, patterns)
-          ) {
-            labels.push(label);
-          }
+    const repoToken: string = core.getInput("repo-token", { required: true });
 
-          return labels;
-        },
-        []
-      );
+    if (context && context.payload && context.payload.repository && context.payload.pull_request) {
+      const octokit = new github.GitHub(repoToken);
+      const repoConfig: ConfigEntry[] = await getConfig(octokit, CONFIG_FILENAME, context.repo);
+      const config: ConfigEntry[] = repoConfig.length > 0 ? repoConfig : defaults;
+      const headRef = context.payload.pull_request.head.ref;
+      const baseRef = context.payload.pull_request.base.ref;
+      const labelsToAdd = config.reduce((labels: string[], entry) => {
+        if (entry.head && entry.base) {
+          if (isMatch(headRef, entry.head) && isMatch(baseRef, entry.base)) {
+            labels.push(entry.label);
+          }
+        } else if (entry.head && isMatch(headRef, entry.head)) {
+          labels.push(entry.label);
+        } else if (entry.base && isMatch(baseRef, entry.base)) {
+          labels.push(entry.label);
+        }
+
+        return labels;
+      }, []);
 
       if (labelsToAdd.length > 0) {
+        core.debug(`Adding labels: ${labelsToAdd}`);
         await octokit.issues.addLabels({
-          number: context.payload.pull_request.number,
+          issue_number: context.payload.pull_request.number,
           labels: labelsToAdd,
-          ...repoInfo
+          ...context.repo
         });
       }
     }
   } catch (error) {
     core.setFailed(error.message);
+    throw error;
   }
+}
+
+function isMatch(ref: string, patterns: string | string[]): boolean {
+  return Array.isArray(patterns)
+    ? patterns.some(pattern => matcher.isMatch(ref, pattern))
+    : matcher.isMatch(ref, patterns);
 }
 
 run();
