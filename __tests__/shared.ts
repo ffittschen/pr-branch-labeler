56397a39e61651d9939663f59c189be2966cbf45
Adam Weeden <adam.weeden@nrccua.org>
2020-08-25 14:15:07 -0400

    Added more spies for core logging in jest

--- __tests__/shared.ts
+++ __tests__/shared.ts
@@ -2,9 +2,11 @@ import * as core from "@actions/core";
 import { readFileSync } from "fs";
 import "jest-extended";
 
-// Dump core debug/error to console debug so that --silent works
+// Dump core messaging to console so that --silent works
 jest.spyOn(core, 'debug').mockImplementation(console.debug);
-jest.spyOn(core, 'error').mockImplementation(console.debug);
+jest.spyOn(core, 'info').mockImplementation(console.info);
+jest.spyOn(core, 'error').mockImplementation(console.error);
+jest.spyOn(core, 'warning').mockImplementation(console.warn);
 
 function encodeContent(content: Buffer | ArrayBuffer | SharedArrayBuffer) {
     return Buffer.from(content).toString("base64");
